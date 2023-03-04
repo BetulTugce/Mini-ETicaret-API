40e552a2ef42fe76e57615b0a3172eb693015ba1
Betul Tugce Dikdogmus <betultugcedikdogmus@gmail.com>
2023-03-04 23:43:29 +0300

    Add project files.

---
+++ Infrastructure/ETicaretAPI.Persistence/ServiceRegistration.cs
@@ -0,0 +1,22 @@
+﻿using ETicaretAPI.Application.Abstractions;
+using ETicaretAPI.Persistence.Concretes;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Persistence
+{
+    public static class ServiceRegistration
+    {
+        //Bu sınıf içerisinde extention fonksiyonlar tanımlanacak.
+        //Presentation katmanındaki IoC containera bu katmandaki servisleri tanıtmamızı sağlayacak.
+
+        public static void AddPersistenceServices(this IServiceCollection services)
+        {
+            services.AddSingleton<IProductService, ProductService>();
+        }
+    }
+}
