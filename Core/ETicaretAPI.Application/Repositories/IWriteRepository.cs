7003b418a151aa1f55a1b12e942e778c9eec7170
Betul Tugce Dikdogmus <betultugcedikdogmus@gmail.com>
2023-03-06 20:32:01 +0300

    Generic Repositoryler yazıldı

--- Core/ETicaretAPI.Application/Repositories/IWriteRepository.cs
+++ Core/ETicaretAPI.Application/Repositories/IWriteRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ETicaretAPI.Domain.Entities.Common;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,12 +7,14 @@ using System.Threading.Tasks;
 
 namespace ETicaretAPI.Application.Repositories
 {
-    public interface IWriteRepository<T> : IRepository<T> where T : class
+    public interface IWriteRepository<T> : IRepository<T> where T : BaseEntity
     {
         Task<bool> AddAsync(T model);
-        Task<bool> AddAsync(List<T> models);
-        Task<bool> RemoveAsync(T model);
+        Task<bool> AddRangeAsync(List<T> models);
+        bool Remove(T model);
+        bool RemoveRange(List<T> models);
         Task<bool> RemoveAsync(string id);
-        Task<bool> UpdateAsync(T model);
+        bool Update(T model);
+        Task<int> SaveAsync();
     }
 }
